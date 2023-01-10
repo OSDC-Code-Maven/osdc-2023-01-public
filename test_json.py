159cf4d69b210f522bd11d3b61586ef48d1e1815
Gabor Szabo <gabor@szabgab.com>
2023-01-10 08:09:35 +0200

    check valid field names; separate url checking
    assuming the tests are executed in this order first the fast checks will
    be executed and only then the slow ones that need network access.

--- test_json.py
+++ test_json.py
@@ -4,14 +4,24 @@ from generate import check_github_acc_for_participant
 GITHUB_URL: str = "https://github.com/"
 GITLAB_URL: str = "https://gitlab.com/"
 
+VALID_FIELDS = ['name', 'linkedin', 'github', 'gitlab', 'devto', 'posts']
+
 def test_json():
     for folder in ['mentors', 'participants']:
         people = read_json_files(folder)
         for person in people:
             for field in person.keys():
                 assert field == field.lower()
+                assert field in VALID_FIELDS
+
             assert 'name' in person
+
             assert 'github' in person
+
+def test_urls():
+    for folder in ['mentors', 'participants']:
+        people = read_json_files(folder)
+        for person in people:
             assert check_github_acc_for_participant(GITHUB_URL + person['github']), f"Checking {GITHUB_URL + person['github']}"
             if 'gitlab' in person:
                 if person['github'] not in ['anatlavitzkovitz']:
