e0421da94be17ab94474c8420e71faa9aa94e3c8
Gabor Szabo <gabor@szabgab.com>
2023-01-10 06:45:45 +0200

    Verify that all the keys in the personal json files are lowercase #15

--- test_json.py
+++ test_json.py
@@ -7,6 +7,8 @@ def test_json():
     for folder in ['mentors', 'participants']:
         people = read_json_files(folder)
         for person in people:
+            for field in person.keys():
+                assert field == field.lower()
             assert 'name' in person
             assert 'github' in person
             assert check_github_acc_for_participant(GITHUB_URL + person['github']), f"Checking {GITHUB_URL + person['github']}"
